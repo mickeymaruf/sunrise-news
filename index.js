f66cf932589a7a722bed657a8e7baf2606653799
Maruf Hossain <maruf.h4x@gmail.com>
2022-11-05 22:15:57 +0600

    create news with datetime

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
 const app = express()
+const datetime = require('node-datetime')
 const port = process.env.PORT || 5000
-
-// const categories = require('./data/categories.json')
 
 // middlewares
 app.use(cors())
-
+app.use(express.json())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWD}@cluster0.ld8a5ol.mongodb.net/?retryWrites=true&w=majority`;
@@ -20,8 +19,11 @@ const run = async () => {
         const categoryCollection = database.collection('categories');
         // news
         app.get('/news', async (req, res) => {
-            const cursor = newsCollection.find({});
-            const news = await cursor.toArray()
+            const options = {
+                sort: { 'author.published_date': -1 }
+            }
+            const cursor = newsCollection.find({}, options);
+            const news = await cursor.toArray();
             res.send(news);
         })
         // single news by id
@@ -31,7 +33,22 @@ const run = async () => {
             const singleNews = await newsCollection.findOne(query);
             res.send(singleNews);
         })
+        app.post('/news', async (req, res) => {
+            const newsObj = req.body;
+            // 
+            newsObj.others_info = { is_todays_pick: false, is_trending: false }
+            newsObj.rating = { number: 0, badge: "" }
+            newsObj.total_view = 0;
 
+            const dt = datetime.create()
+            const formatted = dt.format('Y-m-d H:M:S');
+            newsObj.author.published_date = formatted;
+            // 
+            const result = await newsCollection.insertOne(newsObj);
+            res.send(result);
+        })
+
+        // categories
         app.get('/categories', async (req, res) => {
             const cursor = categoryCollection.find({});
             const categories = await cursor.toArray()
@@ -39,9 +56,12 @@ const run = async () => {
         })
 
         app.get('/category/:_id/', async (req, res) => {
+            const options = {
+                sort: { 'author.published_date': -1 }
+            }
             const _id = req.params._id;
             const query = { category_id: _id };
-            const cursor = newsCollection.find(query);
+            const cursor = newsCollection.find(query, options);
             const news = await cursor.toArray();
             res.send(news);
         })
