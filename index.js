1c855e04533bc6fe4f579b125225afb13f8a452a
Maruf Hossain <maruf.h4x@gmail.com>
2022-11-05 19:07:27 +0600

    migrated data in mongodb - categories and news and replaced news category_id with newly generated category id in mongo atlas

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
 const express = require('express')
+require('dotenv').config()
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 5500
+const port = process.env.PORT || 5000
 
-const categories = require('./data/categories.json')
-const news = require('./data/news.json')
+// const categories = require('./data/categories.json')
 
+// middlewares
 app.use(cors())
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWD}@cluster0.ld8a5ol.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async () => {
+    try {
+        const database = client.db('sunrise-news');
+        const newsCollection = database.collection('news');
+        const categoryCollection = database.collection('categories');
+        // news
+        app.get('/news', async (req, res) => {
+            const cursor = newsCollection.find({});
+            const news = await cursor.toArray()
+            res.send(news);
+        })
+        // single news by id
+        app.get('/news/:_id', async (req, res) => {
+            const _id = req.params._id;
+            const query = { _id: ObjectId(_id) }
+            const singleNews = await newsCollection.findOne(query);
+            res.send(singleNews);
+        })
+
+        app.get('/categories', async (req, res) => {
+            const cursor = categoryCollection.find({});
+            const categories = await cursor.toArray()
+            res.send(categories);
+        })
+
+        app.get('/category/:_id/', async (req, res) => {
+            const _id = req.params._id;
+            const query = { category_id: _id };
+            const cursor = newsCollection.find(query);
+            const news = await cursor.toArray();
+            res.send(news);
+        })
+    } finally {
+        // 
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.get('/categories', (req, res) => {
-    res.send(categories);
-})
-
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    let category_news = news.filter(n => n.category_id === id);
-    if (id === '08') {
-        category_news = news;
-    }
-    res.send(category_news);
-})
-
-app.get('/news', (req, res) => {
-    res.send(news);
-})
-
-app.get('/news/:id', (req, res) => {
-    const id = req.params.id;
-    res.send(news.find(n => n._id === id));
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
